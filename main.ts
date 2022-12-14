45936b421a459e5d789ad04c8acf4c02a9b18117
Elliot Plant <elliotaplant@gmail.com>
2022-12-14 10:44:37 -0800

    Get simple todo's working

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
-import pg from 'pg';
+import connect, { sql } from '@databases/sqlite';
+
+// Creates an in-memory SQLite DB. Note that this DB does not write to disk.
+const db = connect();
+
+async function dbInit() {
+  await db.query(sql`
+    CREATE TABLE IF NOT EXISTS todo (
+      todo_id INTEGER PRIMARY KEY,
+      label VARCHAR NOT NULL,
+      done BOOLEAN NOT NULL DEFAULT FALSE
+    );
+  `);
+}
 
 const app: Express = express();
-const client = new pg.Client({
-  connectionString: 'postgresql://local:local@localhost:5555/interview',
-});
 
 // Set up middlewear
 app.use(cors());
@@ -13,29 +23,27 @@ app.use(express.json());
 
 // Route to get all todos
 app.get('/todo', async (_: Request, res: Response) => {
-  const result = await client.query('SELECT * from todo');
-  res.send(result.rows);
+  const result = await db.query(sql`SELECT * from todo`);
+  res.send(result);
 });
 
 // Route to create a todo
 app.post('/todo', async (req: Request, res: Response) => {
-  const result = await client.query(
-    'INSERT INTO todo (label) VALUES ($1) RETURNING *',
-    [req.body.label]
+  const result = await db.query(
+    sql`INSERT INTO todo (label) VALUES (${req.body.label}) RETURNING *`
   );
-  res.send(result.rows[0]);
+  res.send(result);
 });
 
 // Route to toggle the 'done' state of a todo
-app.put('/todo/:id', async (req: Request, res: Response) => {
-  const result = await client.query(
-    'UPDATE todo SET done = NOT done WHERE id = $1 RETURNING *',
-    [req.params.id]
+app.put('/todo/:todo_id', async (req: Request, res: Response) => {
+  const result = await db.query(
+    sql`UPDATE todo SET done = NOT done WHERE todo_id = ${req.params.todo_id} RETURNING *`
   );
-  res.send(result.rows[0]);
+  res.send(result);
 });
 
 app.listen(8080, async () => {
-  await client.connect();
+  await dbInit();
   console.log('Server is running at http://localhost:8080');
 });
